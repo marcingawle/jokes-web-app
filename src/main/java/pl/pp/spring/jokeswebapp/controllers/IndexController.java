459f9722a6a555e0bdaf81b399b6a34ec3952772
marcingawle <marcingawle@gmail.com>
2021-08-21 09:48:17 +0200

    Using custom banner. closes #1

--- src/main/java/pl/pp/spring/jokeswebapp/controllers/IndexController.java
+++ src/main/java/pl/pp/spring/jokeswebapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package pl.pp.spring.jokeswebapp.controlers;
+package pl.pp.spring.jokeswebapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
