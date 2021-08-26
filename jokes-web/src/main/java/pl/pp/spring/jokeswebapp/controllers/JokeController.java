1f2641b4756d6fc25510b8277df6cfde6251d8c9
marcingawle <marcingawle@gmail.com>
2021-08-26 13:10:59 +0200

    create a form for adding a new joke. #15

--- jokes-web/src/main/java/pl/pp/spring/jokeswebapp/controllers/JokeController.java
+++ jokes-web/src/main/java/pl/pp/spring/jokeswebapp/controllers/JokeController.java
@@ -1,13 +1,24 @@
 package pl.pp.spring.jokeswebapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pp.spring.jokeswebapp.services.CategoryService;
 
 @Controller
 public class JokeController {
 
+    private final CategoryService categoryService;
+
+    public JokeController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @RequestMapping("/jokes/add")
-    public String addJokeForm() {
+    public String addJokeForm(Model model) {
+
+        model.addAttribute("categories", categoryService.findAll());
+
         return "jokes/add";
     }
 }
