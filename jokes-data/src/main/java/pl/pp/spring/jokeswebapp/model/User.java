71acdee597af4f56413ef4c7659f2b1d274f6562
marcingawle <marcingawle@gmail.com>
2021-09-01 14:56:22 +0200

    annotations Table and Column

--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/User.java
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/User.java
@@ -1,17 +1,22 @@
 package pl.pp.spring.jokeswebapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
 }
