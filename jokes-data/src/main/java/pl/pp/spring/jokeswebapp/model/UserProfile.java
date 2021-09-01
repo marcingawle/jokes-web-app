05bdf5fcf04781ad0b15a4381e21db51c4ac031c
marcingawle <marcingawle@gmail.com>
2021-09-01 15:22:00 +0200

    annotation @Lob - CLOB, BLOB

---
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/UserProfile.java
@@ -0,0 +1,71 @@
+package pl.pp.spring.jokeswebapp.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+
+    @Lob
+    private String description;
+
+    @Lob
+    private Byte[] image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+}
