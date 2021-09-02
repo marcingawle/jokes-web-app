713e1414425bdd044efdb7baf39960cfdd45d3c6
marcingawle <marcingawle@gmail.com>
2021-09-02 09:48:58 +0200

    UserService, UserProfileService and map implementations

--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/JokeMapService.java
+++
@@ -1,9 +0,0 @@
-package pl.pp.spring.jokeswebapp.services;
-
-import org.springframework.stereotype.Service;
-import pl.pp.spring.jokeswebapp.model.Joke;
-
-@Service
-public class JokeMapService extends BaseMapService<Joke> implements JokeService{
-
-}
---
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/map/JokeMapService.java
@@ -0,0 +1,10 @@
+package pl.pp.spring.jokeswebapp.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.pp.spring.jokeswebapp.model.Joke;
+import pl.pp.spring.jokeswebapp.services.JokeService;
+
+@Service
+public class JokeMapService extends BaseMapService<Joke> implements JokeService {
+
+}
