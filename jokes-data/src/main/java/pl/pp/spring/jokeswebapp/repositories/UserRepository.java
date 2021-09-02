9e63978a1abdc051a3580d275cd436955de43875
marcingawle <marcingawle@gmail.com>
2021-09-02 13:52:52 +0200

    add user repository

---
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package pl.pp.spring.jokeswebapp.repositories;
+
+import pl.pp.spring.jokeswebapp.model.User;
+
+public interface UserRepository {
+    User save(User user);
+}
--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/map/UserMapService.java
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/map/UserMapService.java
@@ -1,10 +1,15 @@
 package pl.pp.spring.jokeswebapp.services.map;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import pl.pp.spring.jokeswebapp.model.User;
 import pl.pp.spring.jokeswebapp.services.UserService;
 
 @Service
 public class UserMapService extends BaseMapService<User> implements UserService {
-
+    @Override
+    public User save(User entity) {
+        System.out.println("user map service");
+        return super.save(entity);
+    }
 }
