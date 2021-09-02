d180fc8631e64ac7efec93c0acc7cdcf3b111593
marcingawle <marcingawle@gmail.com>
2021-09-02 11:06:44 +0200

    add users list html page

---
+++ jokes-web/src/main/java/pl/pp/spring/jokeswebapp/controllers/UserController.java
@@ -0,0 +1,24 @@
+package pl.pp.spring.jokeswebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pp.spring.jokeswebapp.services.UserService;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @RequestMapping("/users")
+    public String showIndex(Model model) {
+        model.addAttribute("users", userService.findAll());
+
+        return "users/list";
+    }
+}
