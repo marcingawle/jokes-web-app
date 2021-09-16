260f4022e145abbfb2d6146b5137b96f12a65c1b
marcingawle <marcingawle@gmail.com>
2021-09-16 15:37:22 +0200

    profiles

--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/db/JokeDbService.java
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/db/JokeDbService.java
@@ -1,6 +1,7 @@
 package pl.pp.spring.jokeswebapp.services.db;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pl.pp.spring.jokeswebapp.model.Joke;
 import pl.pp.spring.jokeswebapp.repositories.JokeRepository;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Primary
+@Profile("db")
 public class JokeDbService implements JokeService {
 
     private final JokeRepository jokeRepository;
